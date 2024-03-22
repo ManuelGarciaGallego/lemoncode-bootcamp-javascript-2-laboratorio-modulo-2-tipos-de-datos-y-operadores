98cca59add22d21e616a04f4d7b20f21beb4107b
Manuel García Gallego <manuelgalleggo@gmail.com>
2024-03-22 13:13:16 +0100

    modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
-import "./style.css";
-
-console.log("Hello Typescript!");
+console.log(
+  "Yo pagaría los 18€ correspondientes a las bebidas y cada uno de mis seis amigos pagaría " +
+    (120 - 18) / 6 +
+    "€ del resto de la cuenta."
+);
